962d82e9db6c59aa7da477750702f9360035392a
Roman Mavrin <roma-mrv@yandex.ru>
2021-02-16 07:30:37 +0300

    add brackets

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  for (let i = 0; i < str.length; i++){
+ for (let i = 0; i < str.length; i++){
     for(let x = 0; x < bracketsConfig.length; x++)
     {
       if(str[i] == bracketsConfig[x][0] && str[i+1] == bracketsConfig[x][1]){
         str = str.replace(bracketsConfig[x][0]+bracketsConfig[x][1], '');
+        i = -1;
       }
     }
   }
